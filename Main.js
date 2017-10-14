deadcf7426b7c9d9cdaa56d4c88ec7184c933f22
kalairaj1403 <32799147+kalairaj1403@users.noreply.github.com>
2017-10-14 22:10:44 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,6 @@
+ var Arthimetic = require("./Arthimetic");
+
+console.log(Arthimetic.add(2,2));
+console.log(Arthimetic.sub(2,2));
+console.log(Arthimetic.mul(2,2));
+console.log(Arthimetic.div(2,2));
